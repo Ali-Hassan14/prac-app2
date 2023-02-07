2e6ed72aea490af9ac9ff003d66ad4a6c86f6261
AliHassan1407 <alihassan1407@outlook.com>
2023-02-07 23:33:17 +0500

    App Completion Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import React, {useState} from "react";
+import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[usersList,setUsersList]=useState([]);
+  const addUsersHandler=(uName,uAge)=>{
+    setUsersList((prevUsersList)=>{
+      return [
+        ...prevUsersList,
+        {name:uName,age:uAge,id:Math.random().toString()}
+      ];
+    });
+    // console.log(usersList)
+  };
+    return (
+    <div>
+      <AddUser onAddUser={addUsersHandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
